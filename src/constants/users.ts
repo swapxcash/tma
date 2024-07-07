97f18e3dbd08d7bb3482782d98f4c6335855703b
alexpataman <alex.pataman@gmail.com>
2024-07-07 21:51:10 +0200

    -

---
+++ src/constants/users.ts
@@ -0,0 +1,1 @@
+export const allowedUserNicknames = ["alexhofn", "morano85"];
--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,27 +1,39 @@
 import {
   Section,
   Cell,
-  Image,
   List,
   Placeholder,
+  Banner,
+  Modal,
+  Button,
 } from "@telegram-apps/telegram-ui";
 
 import { Link } from "@/components/Link/Link.tsx";
 
-import tonSvg from "./ton.svg";
-
 import "./IndexPage.css";
 import WebApp from "@twa-dev/sdk";
+import { allowedUserNicknames } from "@/constants/users.ts";
+import { BsSignStop } from "react-icons/bs";
 
 export function IndexPage() {
   const { user } = WebApp.initDataUnsafe;
-  console.log("initData", user);
+  if (!user?.username || !allowedUserNicknames.includes(user?.username)) {
+    return (
+      <Banner
+        before={<BsSignStop className="text-red-600 size-10" />}
+        header="You don't have acces to the app"
+        subheader="Contact with support team to get access"
+        type="section"
+      ></Banner>
+    );
+  }
+
   return (
     <List>
       <div className="HIJtihMA8FHczS02iWF5">
         <Placeholder
           description={`Your ID is ${user?.id ?? "unknown"}`}
-          header={`Hi ${user?.username ?? "there"}!`}
+          header={`Hi ${user?.username ?? "there"}`}
         >
           <img
             alt="Telegram sticker"
@@ -30,25 +42,22 @@ export function IndexPage() {
           />
         </Placeholder>
       </div>
-      <Section
-        header="Features"
-        footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-      >
-        <Link to="/ton-connect">
-          <Cell
-            before={
-              <Image src={tonSvg} style={{ backgroundColor: "#007AFF" }} />
-            }
-            subtitle="Connect your TON wallet"
-          >
-            TON Connect
-          </Cell>
-        </Link>
-      </Section>
-      <Section
-        header="Application Launch Data"
-        footer="These pages help developer to learn more about current launch information"
-      >
+      {/*<Section*/}
+      {/*  header="Features"*/}
+      {/*  footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"*/}
+      {/*>*/}
+      {/*  <Link to="/ton-connect">*/}
+      {/*    <Cell*/}
+      {/*      before={*/}
+      {/*        <Image src={tonSvg} style={{ backgroundColor: "#007AFF" }} />*/}
+      {/*      }*/}
+      {/*      subtitle="Connect your TON wallet"*/}
+      {/*    >*/}
+      {/*      TON Connect*/}
+      {/*    </Cell>*/}
+      {/*  </Link>*/}
+      {/*</Section>*/}
+      <Section header="Application Launch Data" footer="Footer message">
         <Link to="/init-data">
           <Cell subtitle="User data, chat information, technical data">
             Init Data
@@ -64,6 +73,22 @@ export function IndexPage() {
             Theme Parameters
           </Cell>
         </Link>
+
+        <div className="flex justify-center py-6">
+          <Modal trigger={<Button size="m">Open modal</Button>}>
+            <Placeholder description="Description" header="Title">
+              <img
+                alt="Telegram sticker"
+                src="https://xelene.me/telegram.gif"
+                style={{
+                  display: "block",
+                  height: "144px",
+                  width: "144px",
+                }}
+              />
+            </Placeholder>
+          </Modal>
+        </div>
       </Section>
     </List>
   );
