30a2af9a3359e616988d6c96a405a86446dfd5fb
alexpataman <alex.pataman@gmail.com>
2024-07-07 21:21:21 +0200

    -

--- src/pages/LaunchParamsPage/LaunchParamsPage.tsx
+++ src/pages/LaunchParamsPage/LaunchParamsPage.tsx
@@ -10,6 +10,7 @@ export function LaunchParamsPage() {
   return (
     <List>
       <DisplayData
+        header="LaunchParamsPage"
         rows={[
           { title: "tgWebAppPlatform", value: WebApp.platform },
           { title: "tgWebAppVersion", value: WebApp.version },
@@ -17,10 +18,9 @@ export function LaunchParamsPage() {
             title: "tgWebAppStartParam",
             value: WebApp.initDataUnsafe?.start_param,
           },
-          { title: "tgWebAppData", type: "link", value: "/init-data" },
+          { title: "tgWebAppData", value: "/init-data" },
           {
             title: "tgWebAppThemeParams",
-            type: "link",
             value: "/theme-params",
           },
         ]}
--- src/pages/ThemeParamsPage/ThemeParamsPage.tsx
+++ src/pages/ThemeParamsPage/ThemeParamsPage.tsx
@@ -7,6 +7,7 @@ export function ThemeParamsPage() {
   return (
     <List>
       <DisplayData
+        header="ThemeParamsPage"
         rows={Object.entries(WebApp.themeParams).map(([title, value]) => ({
           title,
           value,
